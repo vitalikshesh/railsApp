167672646cdb1e7ead997cf035613635dbc75b53
vitalikshesh <vitalikshesh192@gmail.com>
2018-06-06 19:03:15 +0300

    end_9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'store/index'
   resources :outfits
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'store#index', as: 'store'
 end
